851f42ffae9f1cf9c6256efd6542f6284fba9c30
oopsmails <oopsmails@gmail.com>
2024-07-17 16:27:08 -0400

    prettier, styles.scss variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'element-base': `rgba(var(--default-color), <alpha-value)`,
+        'nav-default-height': `var(--nav-bar-height)`
+      }
+    },
   },
   plugins: [],
 }
